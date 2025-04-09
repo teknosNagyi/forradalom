857c882028a3d600f01cc0d747048343658f8fdb
Alex <galalex20160925@gmail.com>
2025-04-09 20:38:17 +0200

    tablazat letrehozasa oop

--- oop/area.js
+++ oop/area.js
@@ -1,7 +1,18 @@
 /**
- * @param {string} className stringet var parameterul
+* megnezi van a containeroop ha nincs csinald egy containert
+ * csinal egy divet es beallitja a class nevet es hozzad adja a containerhez
  */
 class Area{ //letrehozza az Area osztalyt
+    #div // privat div elem
+    /**
+     * @returns {HTMLElement} div
+     */
+    get div(){//letrehozza a div gettert
+        return this.#div;//visszaadja a divet
+    }
+    /**
+    * @param {string} className stringet var parameterul
+    */
     // constructor letrehozasa className parameterrel
     constructor(className){
         // megnezi van e mar containeroop class
@@ -12,8 +23,34 @@ class Area{ //letrehozza az Area osztalyt
             container.className = 'containeroop';//beallitja a class nevet containeroop-ra
             document.body.appendChild(container);//hozzaadja a bodyhoz
         }
-        const div = document.createElement('div'); //letrehoz egy div elemet
-        div.className = className;//aminek a class nevet  classNamere
-        container.appendChild(div);//hozzaadja a divet a containerhez
+        this.#div = document.createElement('div');//letrehozza a divet
+        this.#div.className = className;//beallitja a class nevet
+        container.appendChild(this.#div);//hozzaadja a divet a containerhez
+    }
+}
+
+/**
+ *  az Area osztalybol leszarmazott osztalya es egy tablazatot hoz letre 
+ */
+class Table extends Area {
+    /**
+     * @param {string} Class stringet var parameterul
+     */
+    constructor(Class){//constructor letrehozasa Class parameterrel
+        super(Class); //meghivja a szulo osztaly konstruktorat
+        const table = document.createElement('table');//letrehozza a tablet
+        this.div.appendChild(table);//hozzaadja a tablet a divhez
+        const tableheader = document.createElement('thead');//letrehozza a thead elemet
+        table.appendChild(tableheader);//hozzaadja a thead elemet a tablehoz
+        const tableheaderow = document.createElement('tr');//letrehozza a sort
+        tableheader.appendChild(tableheaderow);//hozzaadja a sort a thead elemhez
+        const theadercella = ['forradalom', 'evszam', 'sikeres'];// letrehoz egy tombot a fejlec cellainak tartalmaibol
+        for (const celltartalom of theadercella) {// a tomb elemein vegigmegyunk
+            const cella = document.createElement('th');// letrehoz egy th cellat
+            cella.innerText = celltartalom;// a cella tartalma a tomb elemei lesznek
+            tableheaderow.appendChild(cella);// hozzaadja a cellat a sorhoz
+        }
+        const tablebody = document.createElement('tbody');// letrehoz egy tbody elemet
+        table.appendChild(tablebody);// hozzaadja a tbody elemet a tablazathoz
     }
 }
--- oop/main.js
+++ oop/main.js
@@ -1,5 +1,5 @@
 const separator = document.createElement("hr");// hogy a html-en egyszeruen megtalalhato legyen az elvalaszto oop es sima kozott
 document.body.appendChild(separator);//hozzaadja a separatort a bodyhoz
 
-const table = new Area('table');//letrehozza a tablet
+const table = new Table('table');//letrehozza a tablet
 const form = new Area('form');//letrehozza a formot
