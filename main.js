aa42dc270b381bc2a08cef26e3996f13630615bb
Alex <galalex20160925@gmail.com>
2025-04-09 20:03:41 +0200

    tablazat hozzaadasa sima

--- main.js
+++ main.js
@@ -18,6 +18,29 @@ const tableDiv = Div("table");
 // Letrehoz egy div elemet form nevvel
 const formDiv = Div("form");
 
+const table1 = document.createElement("table");// letrehoz egy tablazatot
+// hozzaadja a tablazatot a tabledivhez
+tableDiv.appendChild(table1);
+const tablheader=document.createElement("thead");// letrehoz egy thead elemet
+table1.appendChild(tablheader);// hozzaadja a thead elemet a tablazathoz
+const tableheaderow=document.createElement("tr");// letrehoz egy sort a thead elemhez
+tablheader.appendChild(tableheaderow);// hozzaadja a sort a thead elemhez
+
+const theadercella=['forradalom', 'evszam', 'sikeres'];// letrehoz egy tombot a fejlec cellainak tartalmahoz
+// a tomb elemeit vegigmegyunk
+for (const celltartalom of theadercella){
+    const cella=document.createElement("th"); // letrehoz egy th cellat
+    cella.innerText=celltartalom; // a cella tartalma a tomb elemei lesznek
+    tableheaderow.appendChild(cella); // hozzaadja a cellat a sorhoz
+}
+
+const tablebody=document.createElement("tbody"); // letrehoz egy tbody elemet
+table1.appendChild(tablebody); // hozzaadja a tbody elemet a tablazathoz
+
+
+
+
+
 // Hozzaadja a table div elemet a container divhez
 containerDiv.appendChild(tableDiv);
 // Hozzaadja a form div elemet a container divhez
