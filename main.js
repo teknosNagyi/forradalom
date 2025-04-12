541a4588d3758b79a47f94f7640e6f7325efc7d8
Alex <galalex20160925@gmail.com>
2025-04-12 15:11:59 +0200

    form submit hozzaadas es adatok tarolasa

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const array = []; // letrehoz egy ures tombot
 // Egy fuggveny, ami letrehoz egy div elemet a megadott nevvel
 const Div = (className) => {
     // Letrehoz egy uj div elemet
@@ -58,14 +59,62 @@ for (const fieldelem of fieldElementLista) // a tomb elemein vegigmegyunk
     label.htmlFor = fieldelem.fieldid; // beallitja a label htmlfor erteket
     label.textContent = fieldelem.fieldlabel; // beallitja a label szoveget
     field.appendChild(label); // hozzaadja a label elemet a fieldhez
+
     const input = document.createElement('input'); // letrehoz egy input elemet
     input.id = fieldelem.fieldid; // beallitja az input idt
-    field.appendChild(input); // hozzaadja az input elemet a fieldhez
+
+    if(fieldelem.fieldid === 'sikeres') // ha a fieldid sikeres
+    {
+        const input = document.createElement('select'); // letrehoz egy select elemet
+        input.id = fieldelem.fieldid; // beallitja az input idt
+        const option1 = document.createElement('option'); // letrehoz egy option elemet
+        option1.value = 'igen'; // beallitja az option erteket
+        option1.innerText = 'igen'; // beallitja az option szoveget
+        const option2 = document.createElement('option'); // letrehoz egy option elemet
+        option2.value = 'nem'; // beallitja az option erteket
+        option2.innerText = 'nem'; // beallitja az option szoveget
+        input.appendChild(option1); // hozzaadja az option elemet a selecthez
+        input.appendChild(option2); // hozzaadja az option elemet a selecthez
+        field.appendChild(input); // hozzaadja a select elemet a fieldhez
+    }
+    else {
+         const input = document.createElement('input'); // letrehoz egy input elemet
+            input.id = fieldelem.fieldid; // beallitja az input idt
+
+    }
+
+
+     
 } 
 
 const button = document.createElement('button'); // letrehoz egy button elemet
 button.textContent = 'hozzaad'; // beallitja a button tipusat
 form1.appendChild(button); // hozzaadja a button elemet a formhoz
+
+form1.addEventListener('submit', (e) => {// hozzaad egy eseményfigyelot a formhoz
+    e.preventDefault(); // megakadalyozza az alapertelmezett viselkedest
+    const objektumertek = {}; // letrehoz egy ures objektumot
+    const inputelemek= e.target.querySelectorAll('input'); // letrehoz egy tombot az input elemekhez
+    for (const inputelem of inputelemek) { // a tomb elemein vegigmegyunk
+        objektumertek[inputelem.id] = inputelem.value; // beallitja az objektum ertekeit az input elemek id-javal
+    }
+    array.push(objektumertek); // hozzaadja az objektumot a tombhoz
+    const tablebodyrow = document.createElement('tr'); // letrehoz egy sort a tablazathoz
+    tablebody.appendChild(tablebodyrow); // hozzaadja a sort a tablazathoz
+
+    const forradalomcella = document.createElement('td'); // letrehoz egy cellat a tablazathoz
+    forradalomcella.textContent=objektumertek.forradalom // beallitja a cella szoveget az objektum ertekeivel
+    tablebodyrow.appendChild(forradalomcella); // hozzaadja a cellat a sorhoz
+
+    const evszamcella = document.createElement('td'); // letrehoz egy cellat a tablazathoz
+    evszamcella.textContent=objektumertek.evszam // beallitja a cella szoveget az objektum ertekeivel
+    tablebodyrow.appendChild(evszamcella); // hozzaadja a cellat a sorhoz
+
+    const sikerescella = document.createElement('td'); // letrehoz egy cellat a tablazathoz
+    sikerescella.textContent=objektumertek.sikeres // beallitja a cella szoveget az objektum ertekeivel
+    tablebodyrow.appendChild(sikerescella); // hozzaadja a cellat a sorhoz
+} )
+
 
 // Hozzaadja a table div elemet a container divhez
 containerDiv.appendChild(tableDiv);
